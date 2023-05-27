81e858237a1696fedeffceaf090ba01eee9b0c72
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-05-27 12:13:23 +0300

    Update the test fiels

--- solver.rb
+++ solver.rb
@@ -3,9 +3,11 @@ class Solver
         raise ArgumentError, "Invalid input : only accepts 0 and positive integers" if n.negative?
         (1..n).reduce(1,:*)
     end
+
     def reverse(word)
         word.reverse
     end
+    
     def fizzbuzz(n)
     return 'fizzbuzz' if n % 3 == 0 && n % 5 == 0
     return 'fizz' if n % 3 == 0
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -7,12 +7,13 @@ def factorial(n)
     end
 
 describe 'factorial' do
-    it 'returns 1 when the input is 0' do
+    it 'return 1 when the input is 0' do
         expect(factorial(0)).to eq(1)
     end
-    it 'returns the correct factorial' do
+    it 'return the correct factorial' do
         expect(factorial(1)).to eq(1)
         expect(factorial(5)).to eq(120)
+        expect(factorial(2)).to eq(2)
     end
     it 'invalid input or negative values' do
         expect {factorial(-3)}.to raise_error(ArgumentError)
